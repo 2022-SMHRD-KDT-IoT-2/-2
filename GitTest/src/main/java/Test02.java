fb81e1f62dd2849a4f21fea976439be0b96791e3
smhrd <smhrd@DESKTOP-HRK4NUF>
2022-02-22 16:33:50 +0900

    테스트테스트ㅇ

--- GitTest/src/main/java/Test02.java
+++ GitTest/src/main/java/Test02.java
@@ -4,7 +4,9 @@ public class Test02 {
 	public static void main(String[] args) {
 
 System.out.println("Test02 클래스 생성");
-System.out.println("나는야 갓대성 꿇어라");
+System.out.println("나는야 대성");
+System.out.println("나는야 보람상조 팀원");
+
 	}
 
 }
